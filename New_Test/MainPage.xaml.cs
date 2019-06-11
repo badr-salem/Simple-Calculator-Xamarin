cd28f468f44756f73b05fab64cf7cbfc75973712
Badr-Salem <Badrsalem55@gmail.com>
2019-06-11 21:50:41 +0300

    first commit

---
+++ New_Test/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace New_Test
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void Sum(Object sender , EventArgs e)
+        {
+            int x = Convert.ToInt32(txtNum1.Text);
+            int y = Convert.ToInt32(txtNum2.Text);
+            int z = x + y;
+            lblResult.Text = z.ToString();
+        }
+
+        public void Min(Object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(txtNum1.Text);
+            int y = Convert.ToInt32(txtNum2.Text);
+            int z = x - y;
+            lblResult.Text = z.ToString();
+        }
+        public void Multi(Object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(txtNum1.Text);
+            int y = Convert.ToInt32(txtNum2.Text);
+            int z = x * y;
+            lblResult.Text = z.ToString();
+        }
+
+        public void Qsmh(Object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(txtNum1.Text);
+            int y = Convert.ToInt32(txtNum2.Text);
+            int z = x / y;
+            lblResult.Text = z.ToString();
+        }
+    }
+}
